ebac1954ddefa3c7107649632e169ff05f8a532c
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-10 17:09:49 +0530

    Conversion to upper case

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,27 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
-
-    const handleUpClick =() => {
-        // console.log("Uppercase was clicked"  + text);
+    
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
-    }
-    const handleOnChange =(event) => {
-        // console.log("On change");
-        setText(event.target.value);
-    }
-    const [text, setText] = useState('Enter the text here:');
-    
-  return (
-    <div>
-    <h1>{props.heading}</h1>
-    <div className="mb-3">
-      <textarea clasName = "form-control" value = {text} onChange = {handleOnChange} id="myBox" rows = "8"></textarea>
-    </div>
-        <button className="btn btn-primary" onClick = {handleUpClick} >Convert to Upper case</button>
-    </div>
-    
+    };
 
-  )
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    };
+
+    const [text, setText] = useState('Enter the text here:');
+
+    return (
+        <div className="container d-flex flex-column align-items-center mt-4">
+            <h1 className="text-center">{props.heading}</h1>
+            <div className="mb-3 w-75">
+                <textarea 
+                    className="form-control" 
+                    value={text} 
+                    onChange={handleOnChange} 
+                    id="myBox" 
+                    rows="8"
+                ></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>
+                Convert to Uppercase
+            </button>
+        </div>
+    );
 }
