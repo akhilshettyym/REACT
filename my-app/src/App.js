f7e1c45ee6551041b92a5cec6e1aa373adeb393f
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-10 15:00:21 +0530

    Modified commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "Akhil";
 function App() {
   return (
-    <div className="App"></div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>contact</li>
+      <li>Services</li>
+    </nav>
+    <div className = "container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex adipisci nihil debitis accusantium nobis, iusto recusandae est voluptatem quo eaque impedit deleniti labore dicta voluptate! Quas, officiis necessitatibus tenetur dicta impedit minima quisquam quibusdam.</p>
+    </div>
+  
+    </>
+    
+
   );
 }
 
