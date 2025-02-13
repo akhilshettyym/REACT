ef0756b42029f68075b9e64546715f8b6bbb0c80
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-13 15:39:20 +0530

    Navbar upgraded with toggle mode

--- proreact/src/App.js
+++ proreact/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import About from './components/About';
 import Navbar from "./components/Navbar";
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('dark')  //Wheather dark is on or nah
+
+  
   return (
     <>
-      <Navbar title = "Proreact" aboutText = "About"/>
+      <Navbar title = "Proreact" aboutText = "About" mode ={mode} toggleMode={toggleMode}/>
       <div className='container my-3'>
       <TextForm heading = "Enter your text to analyze below:" />
       <About/>
