983e4a4c0316e99fd583a2957d04d92d855ee385
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-14 17:06:15 +0530

    Enabled Alerts.

--- proreact/src/App.js
+++ proreact/src/App.js
@@ -3,27 +3,41 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light');  // Default am setting to light mode traditional way
+  const [alert, setAlert] = useState(null);
 
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#181818';
       document.body.classList.add('dark-mode');
+      showAlert("Dark mode has been enabled", "Success");
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       document.body.classList.remove('dark-mode');
+      showAlert("Light mode has been enabled", "Success");
     }
   };
 
   return (
     <>
       <Navbar title="Proreact" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert = {alert}/>
       <div className='container my-3'>
-        <TextForm heading="Enter your text to analyze below:" mode={mode}/>
+        <TextForm showAlert = {showAlert} heading="Enter your text to analyze below:" mode={mode}/>
         {/* <About/> */}
       </div>
     </>
--- proreact/src/components/TextForm.js
+++ proreact/src/components/TextForm.js
@@ -4,25 +4,30 @@ export default function TextForm(props) {
     
     const handleUpClick = () => {
         setText(text.toUpperCase());
+        props.showAlert("Text converted to uppercase successfully.", "Success");
     };
 
     const handleLoClick = () => {
         setText(text.toLowerCase());
+        props.showAlert("Text converted to lowercase successfully.", "Success");
     };
 
     const handleClearClick = () => {
         setText("");
+        props.showAlert("Input fields have been cleared.", "Success");
     };
 
     const handleCopy = () => {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Modified text copied to clipboard.", "Success");
     };
 
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Extra spaces removed for better readability.", "Success");
     };
 
     const handleOnChange = (event) => {
