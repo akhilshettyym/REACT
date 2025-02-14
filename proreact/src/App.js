097843283b753bbeba986ca0019cd5f66cd2c11b
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-14 20:43:01 +0530

    Final commit

--- proreact/src/App.js
+++ proreact/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from "./components/Navbar";
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -28,16 +28,20 @@ function App() {
   };
 
   return (
-    <Router>
+    // <Router>
+    <>
       <Navbar title="Proreact" aboutText="About" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className='container my-3'>
-        <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your text to analyze below:" mode={mode} />} />
-        </Routes>
+        {/* <Routes> */}
+          {/* <Route exact path="/about" element={<About />} /> */}
+          {/* <Route exact path="/" element={ */}
+            <TextForm showAlert={showAlert} heading="Enter your text to analyze below:" mode={mode} />
+            {/* } /> */}
+        {/* </Routes> */}
       </div>
-    </Router>
+    {/* </Router> */}
+    </>
   );
 }
 
