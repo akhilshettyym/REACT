ec5488d8d63b3c7b8cd639a1853d725b018534f0
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-03 12:14:51 +0530

    Overall weather commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState}  from 'react';
+import axios from 'axios';
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=mangalore&lon=mangalore&appid=d4b93c3c8e14d3568b13df597f90ce97`
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Location: Mangalore</p>
+          </div>
+          <div className="temp">
+            <h1>60°C</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>Feels like: 65°C</p>
+          </div>
+          <div className="humidity">
+            <p>Humidity: 20%</p>
+          </div>
+          <div className="wind">
+            <p>Wind Speed: 10 km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
