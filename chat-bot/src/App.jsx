aa27b3cb04c7a96a0d16ac62122a9bea4626f694
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-08 11:45:46 +0530

    Chat-bot styles import commit

--- chat-bot/src/App.jsx
+++ chat-bot/src/App.jsx
@@ -15,8 +15,22 @@ const App = () => {
       {/* Chatbot Body */}
       <div className="chat-body">
         <div className="message bot-message">
-
+        <ChatbotIcon />
+        <p className="message-text">Hey there <br /> How can I assist you today?</p>
         </div>
+
+        <div className="message user-message">
+        <p className="message-text">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+
+      {/* Chatbot Footer */}
+      <div className="chat-footer">
+        <form action="#" className="chat-form">
+          <input type="text" placeholder="Message..."
+          className="message-input" required />
+          <button className="material-symbols-rounded">arrow_upward</button>
+        </form>
       </div>
     </div>
   </div>
