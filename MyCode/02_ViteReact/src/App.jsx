d2bc1d0338ed07491b7dcf4bb4ef8f33c10e96c2
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-19 17:01:00 +0530

    Overall commit

--- MyCode/02_ViteReact/src/App.jsx
+++ MyCode/02_ViteReact/src/App.jsx
@@ -1,9 +1,12 @@
 import MyCode from "./mycode";
 function App() {
+  const username = ' Me And Code'
   return (
     <>
     <h1>MyCode</h1>
     <MyCode /> 
+    <h1>Me and Code {username}</h1>
+    <p> test para</p>
     </>
   )
 }
--- MyCode/02_ViteReact/src/main.jsx
+++ MyCode/02_ViteReact/src/main.jsx
@@ -2,8 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>My Custom App</h1>
+    </div>
+  )
+}
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Click me to visit Google'
+)
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* Example of rendering a component */}
     <App />
-  </StrictMode>,
+    <MyApp />
+
+    {/* Render element created with React.createElement */}
+    {reactElement}
+
+    {/* Render element created with JSX */}
+    {anotherElement}
+  </StrictMode>
 )
