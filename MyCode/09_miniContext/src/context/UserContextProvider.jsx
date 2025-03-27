a97b2a61566c978291fd65198ab95c766a1734e4
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-27 22:30:11 +0530

    Mini context with UserContextProvider

--- MyCode/09_miniContext/src/context/UserContextProvider.jsx
+++ MyCode/09_miniContext/src/context/UserContextProvider.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import UserContext from "./userContext";
 
-const UserContextProvider = (children) => {
+const UserContextProvider = ( {children} ) => {
     const [user, setUser] = useState(null)
     return (
         <UserContext.Provider value={{user, setUser}}>
