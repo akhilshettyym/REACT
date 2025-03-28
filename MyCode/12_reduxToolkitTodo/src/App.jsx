3f4ca39966f2a17293a5b4b9e067350c637c00e2
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-28 21:29:31 +0530

    Redux Toolkit basic project setup

--- MyCode/12_reduxToolkitTodo/src/App.jsx
+++ MyCode/12_reduxToolkitTodo/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -13,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
