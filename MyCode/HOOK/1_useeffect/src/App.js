a56dcf10ff40d8b384b2e563dd2bcb504068f0a1
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-06-22 23:16:07 +0530

    Change Me useState

--- MyCode/HOOK/1_useeffect/src/App.js
+++ MyCode/HOOK/1_useeffect/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const counter = "Red"
+  // const color = "Red"
+  const [ color, setColor] = useState('Red')
+  const changeMe = () => {
+    setColor('Black')
+  }
 
   return (
     <>
-    <h1>I am React useEffect in white</h1>
+    <h1>I am React useEffect in {color}</h1>
+    <button onClick={changeMe}>Black</button>
     </>
   );
 }
