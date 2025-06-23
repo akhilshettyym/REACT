7b44f4cf660b3689bb072eba0e6f2b9512ab3b83
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-06-23 12:04:15 +0530

    Updated code

--- MyCode/HOOK/1_usestate/src/App.js
+++ MyCode/HOOK/1_usestate/src/App.js
@@ -1,30 +1,3 @@
-// import './App.css';
-// import { useState } from 'react';
-
-// function App() {
-//   const color = "Red"
-//   const [ black, setBlack] = useState({color})
-//   const [ blue, setBlue] = useState({color})
-  
-//   const changeMeBlack = () => {
-//     setBlack('Black')
-//   }
-
-//   const changeMeBlue = () => {
-//     setBlue('Blue')
-//   }
-
-//   return (
-//     <>
-//     <h1>I am React useEffect in {color}</h1>
-//     <button onClick={changeMeBlack}>Black</button>
-//     <button onClick={changeMeBlue}>Blue</button>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 import { useState } from 'react';
 
