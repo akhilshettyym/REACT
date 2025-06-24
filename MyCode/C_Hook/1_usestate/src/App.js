7f8bf93731d753aa69c7c96421cedebbd9eb6209
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-06-24 12:19:08 +0530

    Renamed folder

---
+++ MyCode/C_Hook/1_usestate/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const color = "Red";
+
+  const [black, setBlack] = useState(color);
+  const [blue, setBlue] = useState(color);
+  const [purple, setPurple] = useState(color);
+
+  const changeMeBlack = () => {
+    setBlack('Black');
+  };
+
+  const changeMeBlue = () => {
+    setBlue('Blue');
+  };
+
+  const changeMePurple = () => {
+    setPurple('Purple');
+  };
+
+  return (
+    <>
+      <h1>I am React useState in {color}</h1>
+      <p>Black Color: {black}</p>
+      <p>Blue Color: {blue}</p>
+      <p>Purple Color: {blue}</p>
+      <button onClick={changeMeBlack}>Black</button>
+      <button onClick={changeMeBlue}>Blue</button>
+      <button onClick={changeMePurple}>Purple</button>
+    </>
+  );
+}
+
+export default App;
