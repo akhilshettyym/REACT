11f25b5682cead01e52d68dcd66a45dc50ea05df
akhilshettyym <akhilshettym2003@gmail.com>
2025-06-30 15:31:30 +0530

    Use Effect commit

--- MyCode/C_Hooks/B_useEffect/src/App.jsx
+++ MyCode/C_Hooks/B_useEffect/src/App.jsx
@@ -1,57 +1,14 @@
 import { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
 
 function App() {
-  const color = "Red";
 
   useEffect(() => {
     alert("Welcome to React useState Hook Example");
   }, []);
 
-
-  const [black, setBlack] = useState(color);
-  const [blue, setBlue] = useState(color);
-  const [purple, setPurple] = useState(color);
-
-  const [count, setCount] = useState(0);
-
-  const changeMeBlack = () => {
-    setBlack('Black');
-  };
-
-  const changeMeBlue = () => {
-    setBlue('Blue');
-  };
-
-  const changeMePurple = () => {
-    setPurple('Purple');
-  };
-
-  const changeMe = () => {
-    setCount(count + 1);
-  }
-
-  useEffect(() => {
-    alert("Count was updated");
-  }, [count]);
-  // The above useEffect will render twice because of strict mode in React 18.
-
   return (
     <>
-      <Navbar color={"navy" + " blue"} />
-      <h1>I am React useState in {color}</h1>
-      <p>Black Color: {black}</p>
-      <p>Blue Color: {blue}</p>
-      <p>Purple Color: {purple}</p>
-
-      <button onClick={changeMeBlack}>Black</button>
-      <button onClick={changeMeBlue}>Blue</button>
-      <button onClick={changeMePurple}>Purple</button>
-
-      <div>The count is {count}</div>
-      {/* <button onClick={()=>{setCount(count+1)}}>Update Count</button> */}
-      <button onClick={changeMe}>Update Count</button>
-
+      <h1>I am React useEffect</h1>
     </>
   );
 }
