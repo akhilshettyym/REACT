d37994c4839540c6ee49ad7a3b5bfefb8eef0c19
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-18 17:50:12 +0530

    Ice cream added

--- Redux/redux-demo/index.js
+++ Redux/redux-demo/index.js
@@ -1,6 +1,7 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
+// CAKE
 const BUY_CAKE = 'BUY_CAKE'
 function buyCake() {
   return {
@@ -8,10 +9,19 @@ function buyCake() {
     info: 'First redux action'
   }
 }
+// ICE CREAM
+const BUY_ICE = 'BUY_ICE'
+function buyIce() {
+  return {
+    type: BUY_ICE,
+    info: 'Redux action with ice'
+  }
+}
 
 // (prevState, action) => newState
 const initialState = {
-  numOfCakes: 10
+  numOfCakes: 10,
+  numOfIce: 12
 }
 
 const reducer = (state = initialState, action) => {
@@ -19,6 +29,10 @@ const reducer = (state = initialState, action) => {
     case BUY_CAKE: return {
       ...state,
       numOfCakes: state.numOfCakes - 1
+    }
+    case BUY_ICE: return {
+      ...state,
+      numOfIce: state.numOfIce - 1
     }
     default: return state
   }
@@ -28,8 +42,12 @@ const store = createStore(reducer)
 console.log("Inital state", store.getState());
 const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState()))
 store.dispatch(buyCake())
+store.dispatch(buyIce())
 store.dispatch(buyCake())
+store.dispatch(buyIce())
 store.dispatch(buyCake())
+store.dispatch(buyIce())
+
 unsubscribe()
 
 console.log("BANKAI");
