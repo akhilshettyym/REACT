c57a3e423f172e0b49084a07e4a233edb3c9b615
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-19 17:46:11 +0530

    Overall commit

--- Redux/cake-shop/src/App.js
+++ Redux/cake-shop/src/App.js
@@ -4,11 +4,13 @@ import CakeContainer from './components/CakeContainer';
 import IceContainer from './components/iceContainer';
 import HooksCakeContainer from './components/hooksCakeContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+// import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        {/* <ItemContainer cake/> */}
         <CakeContainer />
         <IceContainer />
         <HooksCakeContainer />
